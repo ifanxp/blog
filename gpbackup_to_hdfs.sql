6ac61926858462fac36490a456ad38155a6886c7
ifanxp <ifanxp@gmail.com>
2021-10-25 15:43:52 +0800

    Update gpbackup_to_hdfs.sql

--- gpbackup_to_hdfs.sql
+++ gpbackup_to_hdfs.sql
@@ -1,3 +1,8 @@
+/*
+in_tablename: 表名
+in_filter: 分区条件
+in_backup_ts: hive 时间戳
+*/
 CREATE OR REPLACE FUNCTION "public"."hivebakup"("in_tablename" text, "in_filter" text, "in_backup_ts" text, "in_debug" int4)
   RETURNS "pg_catalog"."varchar" AS $BODY$
 declare v_sql text;
@@ -10,6 +15,11 @@ declare v_hive_table text = v_server_name||'__'||current_database()||'__'||repla
 declare v_hdfs_location text = 'gphdfs://nameservice1/user/irsuser/gpbackup.db/'||v_hive_table||'/backup_ts='||in_backup_ts||'/backup_'||in_filter||'?compress=true';
 declare v_hive_partname text = 'backup_'||(string_to_array(in_filter, '='))[1];
 BEGIN
+
+/*
+调用方式：
+select public.hivebakup('om_douyin.bt_douyin_mac_video_behavior_2019', 'date_id=20190101', '20211024_1643', 0);
+*/
 
 
 SELECT string_agg(attname || ' ' || pg_type_name, ', '   order by attnum) as pg_define, 
